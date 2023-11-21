ceae4a4c111c22b0c48b086ad4aa7e52dfc0f330
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-11-21 19:31:04 +0100

    optimised: onload, input.values = today and today + 1, if inside if removed

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
 const outboundInput = document.getElementById('outbound-input');
 const returnInput = document.getElementById('return-input');
 
+// Transform a ISO date in date formated for our input date
+// ======================================================
+const formatDate = function (date) {
+    return date.toISOString().split("T")[0];
+}
+// Used for both eventlistener
+// ======================================================
+const currentDate = new Date();
+const currentDatePlusOne = new Date();
+currentDatePlusOne.setDate(currentDatePlusOne.getDate() + 1);
+
+// set currentdate and next day to our inputs
+// ======================================================
+function setDefaultDates() {
+    outboundInput.value = formatDate(currentDate);
+    returnInput.value = formatDate(currentDatePlusOne);
+}
+
+setDefaultDates();
+
+// Input outbound only >= today 
+// ======================================================
+
 outboundInput.addEventListener('change', (e) => {
-    // console.log(e.target.value);
-
-    const currentDate = new Date();
-    // console.log(currentDate)
-
-    const currentDatePlusOne = new Date();
-    currentDatePlusOne.setDate(currentDatePlusOne.getDate() + 1);
-    // console.log(currentDatePlusOne)
 
     const outboundDate = new Date(e.target.value);
-    // console.log(outboundDate)
 
     const outboundDatePlusOne = new Date(e.target.value);
     outboundDatePlusOne.setDate(outboundDatePlusOne.getDate() + 1);
-    // console.log(currentDatePlusOne)
 
     if (outboundDate < currentDate) {
-        outboundInput.value = currentDate.toISOString().split("T")[0];
-        // console.log(("if"))
-        if (returnInput.value.length == 0) {
-            returnInput.value = currentDatePlusOne.toISOString().split("T")[0];
-            // console.log("if if")
-        }
-    } else if (outboundInput.value > returnInput.value) {
-        returnInput.value = outboundDatePlusOne.toISOString().split("T")[0];
-        // console.log("elseif", returnInput, outboundDatePlusOne)
+        outboundInput.value = formatDate(currentDate);
+        //if (returnInput.value.length == 0) { returnInput.value = formatDate(currentDatePlusOne);console.log("if if") }
+    } else if (outboundInput.value >= returnInput.value) {
+        returnInput.value = formatDate(outboundDatePlusOne);
     }
 });
 
+// Input return only >= today +1 , or current day + 1 
+// ======================================================
 returnInput.addEventListener('change', (e) => {
-    const currentDate = new Date();
-    // console.log(currentDate)
-
-    const currentDatePlusOne = new Date();
-    currentDatePlusOne.setDate(currentDatePlusOne.getDate() + 1);
-    // console.log(currentDatePlusOne)
-
     const returnDate = new Date(e.target.value);
-    // console.log(returnDate)
 
     const returnDateMinusOne = new Date(e.target.value);
     returnDateMinusOne.setDate(returnDateMinusOne.getDate() - 1);
 
-    // console.log(currentDatePlusOne)
     if (returnDate <= currentDatePlusOne) {
-        returnInput.value = currentDatePlusOne.toISOString().split("T")[0];
-        // console.log(("if"))
-        if (outboundInput.value.length == 0) {
-            outboundInput.value = currentDate.toISOString().split("T")[0];
-            // console.log("if if")
-        }
-    } else if (returnInput.value < outboundInput.value) {
-        outboundInput.value = returnDateMinusOne.toISOString().split("T")[0];
-        // console.log("elseif", returnInput, outboundDatePlusOne)
+        returnInput.value = formatDate(currentDatePlusOne);
+        // console.log(("if")) // if (outboundInput.value.length == 0) {outboundInput.value = formatDate(currentDate); console.log("if if")}
+    } else if (returnInput.value <= outboundInput.value) {
+        outboundInput.value = formatDate(returnDateMinusOne);
     }
-
 });
 
 
